c208340141857f91c37b1edd515376f71b9b53a8
Rolph DZOUNGA <rolphfrancel@gmail.com>
2016-01-01 08:57:39 +0100

    Finished first cut of the User model

--- Gemfile
+++ Gemfile
@@ -16,8 +16,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 
 group :development, :test do
-  gem "rspec-rails", require: "rspec/rails"
-  
   gem 'byebug'
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,11 @@
 #
 
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
-  	@titre = "Inscription"
+    @titre = "S'inscrire"
   end
 end
