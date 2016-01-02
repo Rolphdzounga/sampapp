3239a28c362ff630d1f9d971e6d2cb0065617924
Rolph DZOUNGA <rolphfrancel@gmail.com>
2016-01-02 12:04:02 +0100

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	@titre = "Profile"
   end
+  def create
+	@user = User.new(user_params)
+	if @user.save
+	log_in @user
+	flash[:success] = "Welcome to the Sample App!"
+	redirect_to @user
+	else
+	render 'new'
+    end
+  end
+
+private
+
+  def user_params
+	params.require(:user).permit(:name, :email, :password,
+	:password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'login'=> 'sessions#new'
+  post 'login'=> 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   get 'users/new'
 
   resources :users
@@ -14,6 +19,7 @@ Rails.application.routes.draw do
   get 'home' => 'pages#home' , to: 'home'
   get 'contact' => 'pages#contact' , to: 'contact'
   get 'about' => 'pages#about' , to: 'about'
+  get 'help' => 'pages#help' , to: 'help'
 
   root 'pages#home'
 end
